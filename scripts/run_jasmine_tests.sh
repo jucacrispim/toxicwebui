8d7fa38caca5b9873204378bd7daf5381a6ad3d0
Juca Crispim <jucacrispim@gmail.com>
2024-06-15 16:22:29 -0300

    Fix jasmine test runner
    ci: include-builders Selenium

--- scripts/run_jasmine_tests.sh
+++ scripts/run_jasmine_tests.sh
@@ -2,7 +2,7 @@
 
 export DISPLAY=:99
  Xvfb :99  -ac -screen 0, 1368x768x24 &
-python ./toxicjasmine/__init__.py ci -c tests/js-unit/jasmine.yml --browser chrome --options=no-sandbox,disable-dev-shm-usage,headless,remote-debugging-port=9222,disable-gpu
+toxicjasmine ci tests/js-unit/SpecRunner.html --browser chrome --options=no-sandbox,disable-dev-shm-usage,headless,remote-debugging-port=9222,disable-gpu
 status=$?
  killall Xvfb
  rm -f /tmp/.X99-lock
